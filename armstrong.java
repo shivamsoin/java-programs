b4107d075421f14cfc6e7a9e3a95eaf17a88f199
om0004 <60003596+om0004@users.noreply.github.com>
2020-10-02 10:07:01 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class armstrong
+{
+	public static void main(String args[])
+	{
+		int c,d,rem,sum=0;
+		armstrong ob=new armstrong();
+		Scanner a=new Scanner(System.in);
+		System.out.print("enter number:");
+		int b=a.nextInt();
+		c=b;
+		d=ob.digits(b);
+		while(b>0)
+		{
+			rem=b%10;
+			sum=sum+(int)(Math.pow(rem,d));
+			b=b/10;
+		}
+		if(sum==c)
+		{
+			System.out.print("armstrong!!");
+		}
+		else
+		{
+			System.out.print("not armstrong!!");
+		}
+		
+		
+		
+	}
+	int digits(int a)
+	{
+		int c=0;
+		while(a>0)
+		{
+			c++;
+			a=a/10;
+		}
+		return c;
+		
+	}
+}
