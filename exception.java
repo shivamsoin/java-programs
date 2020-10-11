78109d5d8749586396836f29d41fe6177cb8cab8
om0004 <60003596+om0004@users.noreply.github.com>
2020-10-11 09:51:44 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,19 @@
+class exception
+{
+	public static void main(String args[])
+	{
+		
+			int a=100,b=0,c;
+			try
+		{
+			c=a/b;
+			System.out.print(c);
+		}
+		
+		catch(Exception e)
+		{
+			System.out.print("sorry not possible");
+		}
+		System.out.print("hi");
+	}
+}
